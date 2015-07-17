2e25576f381f408d891342141e05a88b2bc8a36f
Rob Stiles <robs@codexsoftware.co.uk>
2015-07-17 16:40:23 +0100

    Tweaked entrypoint to ensure touching of crontab files always runs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,12 +3,11 @@ set -e
 
 SETUP_SCRIPT=/root/setup.sh
 
+# Set # of hard links to 1 to keep cron happy.
+touch /etc/cron.d/php5 /var/spool/cron/crontabs/www-data /etc/crontab
+
 if [ -x $SETUP_SCRIPT ]; then
 	echo 'Running one-time setup.'
-
-	# Set # of hard links to 1 to keep cron happy.
-	touch /etc/cron.d/php5 /var/spool/cron/crontabs/www-data /etc/crontab
-
 	source $SETUP_SCRIPT
 	rm -f $SETUP_SCRIPT
 fi
